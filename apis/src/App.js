ae853fd1c5be46d07ab261790d7329381071f167
avalojandro <avalojandro@gmail.com>
2019-08-21 17:12:34 -0600

    API React

---
+++ apis/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+import UserBox from './components/UserBox';
+
+
+import { Container, Heading, Section, Content, Box, Media, Image } from 'react-bulma-components/full';
+
+class App extends Component {  
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: [],
+      isLoaded: false,
+    }
+  }
+
+  async componentDidMount() {
+    await axios.get('https://api.github.com/users/lekastillo')
+    .then(response => {
+        console.log(response)
+        this.setState({
+          user: response.data,
+          isLoaded: true,
+        });
+    })
+    .catch(error => console.log(error))
+  }
+  render() {
+    let { user=[], isLoaded } = this.state;
+    return (
+      <div className="App">
+        <Section>
+          <Container fluid>
+              <Heading size={5} renderAs="p">
+                My Gits Profile
+              </Heading>
+              <Heading subtitle renderAs="p">
+                Using  with React and Axios
+              </Heading>
+          </Container>
+        </Section>
+        <Section>
+          <Container fluid>
+            {isLoaded && <UserBox user={user} />}
+          </Container>
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
